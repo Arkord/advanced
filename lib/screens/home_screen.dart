35d1598b36f8b996de083b5866053dfae8445e8c
arkord <arkord1@gmail.com>
2022-06-15 16:45:21 -0500

    Navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'listview_screen.dart';
 
 class HomeScreen extends StatelessWidget {
    
@@ -6,10 +8,24 @@ class HomeScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('HomeScreen'),
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: const Text("Flutter Advanced Design"),
+        elevation: 0,
       ),
+      body: ListView.separated(
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon(Icons.access_time_filled_outlined),
+          title: const Text("Route name"),
+          onTap: () {
+            final route = MaterialPageRoute(builder: (context) => const ListView2Screen());
+            Navigator.push(context, route);
+          },
+        ), 
+        separatorBuilder: (context, index) => const Divider(), 
+        itemCount: 10
+      )
     );
   }
 }
