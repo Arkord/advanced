0b89a561521e633079b1942a118ad8f5aa514df9
Octavio Mejia <arkord1@gmail.com>
2022-06-20 16:21:30 -0500

    named routes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,31 +1,29 @@
-
 import 'package:flutter/material.dart';
 import 'listview_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pink,
-        title: const Text("Flutter Advanced Design"),
-        elevation: 0,
-      ),
-      body: ListView.separated(
-        itemBuilder: (context, index) => ListTile(
-          leading: const Icon(Icons.access_time_filled_outlined),
-          title: const Text("Route name"),
-          onTap: () {
-            final route = MaterialPageRoute(builder: (context) => const ListView2Screen());
-            Navigator.push(context, route);
-          },
-        ), 
-        separatorBuilder: (context, index) => const Divider(), 
-        itemCount: 10
-      )
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pink,
+          title: const Text("Flutter Advanced Design"),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  leading: const Icon(Icons.access_time_filled_outlined),
+                  title: const Text("Route name"),
+                  onTap: () {
+                    // final route = MaterialPageRoute(builder: (context) => const ListView2Screen());
+                    // Navigator.push(context, route);
+                    //Navigator.pushReplacement(context, newRoute)
+                    Navigator.pushNamed(context, 'card');
+                  },
+                ),
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: 10));
   }
 }
